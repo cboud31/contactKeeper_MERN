ddbd3e806a89bca87c099fa2fa8aa3e90d77241e
Cory Boudreaux <cboud31@gmail.com>
2021-07-01 11:42:23 -0500

    Connect database to app.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const server = express();
+
+//Connect to database
+connectDB();
 
 server.get("/", (req, res) =>
   res.json({ message: `Welcome to the Contact Keeper API!` })
